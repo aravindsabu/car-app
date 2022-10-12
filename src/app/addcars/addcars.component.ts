002191c8338c462ab3f86bb18091a9f77af8859b
aravindsabu <aravindsabu2001@gmail.com>
2022-10-12 13:19:08 +0530

    ok

---
+++ src/app/addcars/addcars.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addcars',
+  templateUrl: './addcars.component.html',
+  styleUrls: ['./addcars.component.css']
+})
+export class AddcarsComponent implements OnInit {
+
+  constructor() { }
+  add:any={}
+
+  ngOnInit(): void {
+  }
+
+}
